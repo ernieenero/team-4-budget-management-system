44125290d49d2e1aa48e759a2e066f8877a49135
Henrick <johnhenrickabistado12@gmail.com>
2022-01-26 19:23:08 +0800

    updated user-homepage

--- application/views/app/user-homepage.php
+++ application/views/app/user-homepage.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="../../../public/css/user-homepage.css">
+    <link rel="stylesheet" href="<?php echo base_url('/public/css/user-homepage.css'); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Document</title>
     
@@ -29,9 +29,12 @@
     <button id="openbtn" class="openbtn" onclick="openNav()">☰</button>  
     <div class="main-page container-fluid" id="main-page">
       <!--<img class="img-fluid rounded" src="images/sched.png" alt="sched">-->
-      <p class="account-name float-left">Account Name <i class="fa fa-edit"></i></p>
-      <div class="user-btn"><a href=""><i class="fa fa-user-circle"></i>User</a></div>
-      <div class="total-expenses">Total Expenses: 1231</div>
+      <p class="account-name">Account Name <i class="fa fa-edit"></i></p>
+      <div class="user-btn">
+        <a href=""><i class="fa fa-user-circle"></i>User</a>
+        <div class="total-expenses">Total Expenses: 1231</div>
+      </div>
+      
       <div class="content">
         <div id="left-content" class="left-content">
             <div class="account-balance">Account Balance: 1234</div>
@@ -82,18 +85,19 @@
 <script>
 function openNav() {
   document.getElementById("mySidebar").style.width = "265px";
+
   document.getElementById("main").style.marginLeft = "265px";
   document.getElementById("openbtn").style.display = "none";
-  document.getElementById("main").style.paddingTop = "60px";
-  document.getElementById("left-content").style.marginLeft = "0px";
+  
 }
 
 function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("mySidebar").style.width = "0px";
   document.getElementById("main").style.marginLeft= "0";
+  
   document.getElementById("openbtn").style.display = "";
-  document.getElementById("main").style.paddingTop = "0";
-  document.getElementById("left-content").style.marginLeft = "15rem";
+  
+  
 }
 </script>
 </html>
