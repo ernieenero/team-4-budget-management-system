df10e7e9be630b39b6b06dd4dc4f108528982f7d
ernieenero1121 <ernieenero1121@gmail.com>
2022-01-27 15:10:33 +0800

    added login tentative

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -13,7 +13,18 @@ class User extends CI_Controller {
         $this->load->view('templates/footer.php');
     }
     public function login(){
-        
+        $this->form_validation->set_rules('username', 'Username', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        if($this->form_validation->run()==TRUE){
+            $data = $this->input->post();
+            if(isset($data['submit'])){
+                unset($data['submit']);
+                $this->load->model('user_model');
+                if($this->user_model->login($data)){
+                    return redirect('homepage');
+                }
+            }
+        }
         $this->load->view('templates/header.php');
         $this->load->view('app/login-page');
         $this->load->view('templates/footer.php');
@@ -21,9 +32,12 @@ class User extends CI_Controller {
     public function signup(){
         
         $this->form_validation->set_rules('username', 'Username', 'required|is_unique[users.username]', array(
-            'required'      => 'You have not provided %s.',
+            'required'      => 'Please Provide %s.',
             'is_unique'     => 'This %s already exists.'
-    ));
+        ));
+        $this->form_validation->set_rules('password', 'Password', 'required', array(
+            'required'=> 'Please Provide %s.',
+        ));
         if($this->form_validation->run() == TRUE){
             $data = $this->input->post();
             if(isset($data['submit'])){
